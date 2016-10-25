e0504049d36fb511e2cc009607f968345edd7106
Sergey Seroukhov <seroukhov@gmail.com>
2016-10-25 13:24:14 -0500

    Rebuilt

--- dist/pip-webui-lists.js
+++ dist/pip-webui-lists.js
@@ -22,11 +22,6 @@ module.run(['$templateCache', function($templateCache) {
 }]);
 })();
 
-/**
- * @file Registration of all WebUI list controls
- * @copyright Digital Living Software Corp. 2014-2016
- */
-/* global angular */
 (function () {
     'use strict';
     angular.module('pipLists', [
@@ -34,11 +29,6 @@ module.run(['$templateCache', function($templateCache) {
     ]);
 })();
 
-/**
- * @file Optional filter to translate string resources
- * @copyright Digital Living Software Corp. 2014-2016
- */
-/* global angular */
 (function () {
     'use strict';
     var thisModule = angular.module('pipList.Translate', []);
@@ -51,22 +41,9 @@ module.run(['$templateCache', function($templateCache) {
     }]);
 })();
 
-/**
- * @file Tag list control
- * @copyright Digital Living Software Corp. 2014-2015
- * @todo
- * - Improve samples in sampler app
- * - What's pipType and pipTypeLocal? Give better name
- * - Do not use ng-if, instead generate template statically
- */
 (function () {
     'use strict';
     var thisModule = angular.module('pipTagList', ['pipList.Translate']);
-    /**
-     * pipTags - set of tags
-     * pipType - additional type tag
-     * pipTypeLocal - additional translated type tag
-     */
     thisModule.directive('pipTagList', ['$parse', function ($parse) {
         return {
             restrict: 'EA',
@@ -80,7 +57,6 @@ module.run(['$templateCache', function($templateCache) {
                 var tagsGetter;
                 tagsGetter = $parse($attrs.pipTags);
                 $element.css('display', 'block');
-                // Set tags
                 $scope.tags = tagsGetter($scope);
                 function toBoolean(value) {
                     if (value == null)
@@ -90,13 +66,11 @@ module.run(['$templateCache', function($templateCache) {
                     value = value.toString().toLowerCase();
                     return value == '1' || value == 'true';
                 }
-                // Also optimization to avoid watch if it is unnecessary
                 if (toBoolean($attrs.pipRebind)) {
                     $scope.$watch(tagsGetter, function () {
                         $scope.tags = tagsGetter($scope);
                     });
                 }
-                // Add class
                 $element.addClass('pip-tag-list');
             }]
         };
