a4e80f2200be1f1e2fdf3c2b1022d0d8f940e95c
Sergey Seroukhov <seroukhov@gmail.com>
2016-10-29 15:56:07 -0500

    Rebuilt

---
+++ dist/pip-webui-lists.js
@@ -0,0 +1,84 @@
+(function(module) {
+try {
+  module = angular.module('pipLists.Templates');
+} catch (e) {
+  module = angular.module('pipLists.Templates', []);
+}
+module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('tag_list/tag_list.html',
+    '<div class="pip-chip rm4 pip-type-chip pip-type-chip-left {{\'bg-\' + pipType + \'-chips\'}}"\n' +
+    '     ng-if="pipType && !pipTypeLocal">\n' +
+    '\n' +
+    '    <span>{{pipType.toUpperCase() | translate | uppercase}}</span>\n' +
+    '</div>\n' +
+    '<div class="pip-chip rm4 pip-type-chip pip-type-chip-left {{\'bg-\' + pipType + \'-chips\'}}"\n' +
+    '     ng-if="pipType && pipTypeLocal">\n' +
+    '\n' +
+    '    <span>{{pipTypeLocal.toUpperCase() | translate | uppercase}}</span>\n' +
+    '</div>\n' +
+    '<div class="pip-chip rm4" ng-repeat="tag in pipTags">\n' +
+    '    <span>{{::tag}}</span>\n' +
+    '</div>');
+}]);
+})();
+
+
+
+(function () {
+    'use strict';
+    angular.module('pipLists', [
+        'pipTagList'
+    ]);
+})();
+
+(function () {
+    'use strict';
+    var thisModule = angular.module('pipList.Translate', []);
+    thisModule.filter('translate', ['$injector', function ($injector) {
+        var pipTranslate = $injector.has('pipTranslate')
+            ? $injector.get('pipTranslate') : null;
+        return function (key) {
+            return pipTranslate ? pipTranslate.translate(key) || key : key;
+        };
+    }]);
+})();
+
+(function () {
+    'use strict';
+    var thisModule = angular.module('pipTagList', ['pipList.Translate']);
+    thisModule.directive('pipTagList', ['$parse', function ($parse) {
+        return {
+            restrict: 'EA',
+            scope: {
+                pipTags: '=',
+                pipType: '=',
+                pipTypeLocal: '='
+            },
+            templateUrl: 'tag_list/tag_list.html',
+            controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
+                var tagsGetter;
+                tagsGetter = $parse($attrs.pipTags);
+                $element.css('display', 'block');
+                $scope.tags = tagsGetter($scope);
+                function toBoolean(value) {
+                    if (value == null)
+                        return false;
+                    if (!value)
+                        return false;
+                    value = value.toString().toLowerCase();
+                    return value == '1' || value == 'true';
+                }
+                if (toBoolean($attrs.pipRebind)) {
+                    $scope.$watch(tagsGetter, function () {
+                        $scope.tags = tagsGetter($scope);
+                    });
+                }
+                $element.addClass('pip-tag-list');
+            }]
+        };
+    }]);
+})();
+
+
+
+//# sourceMappingURL=pip-webui-lists.js.map
