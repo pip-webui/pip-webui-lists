b54cb050de544b4bb4c61a61f2ad45db9f466ba1
KateNegrienko <katya@negrienko.com>
2017-03-02 12:49:27 +0200

    Migration for scss

--- build.conf.js
+++ build.conf.js
@@ -11,7 +11,7 @@ module.exports = {
         tsd: true,
         bundle: true,
         html: true,
-        less: true,
+        sass: true,
         lib: true,
         images: false,
         dist: false
