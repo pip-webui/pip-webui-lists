6c20fb34bb3d01b51bbab85951d27990cbab50e5
Sergey Seroukhov <seroukhov@gmail.com>
2017-03-09 10:31:11 -0700

    Rebuilt

--- dist/pip-webui-lists.js
+++ dist/pip-webui-lists.js
@@ -1,37 +1,24 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}(g.pip || (g.pip = {})).lists = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-<<<<<<< HEAD
-translate.$inject = ['$injector'];
-function translate($injector) {
-    var pipTranslate = $injector.has('pipTranslate')
-        ? $injector.get('pipTranslate') : null;
-    return function (key) {
-        return pipTranslate ? pipTranslate.translate(key) || key : key;
-    };
-}
-angular
-    .module('pipList.Translate', [])
-    .filter('translate', translate);
-
-=======
 (function () {
-    'use strict';
-    filerTranslate.$inject = ['$injector'];
-    function filerTranslate($injector) {
+    translate.$inject = ['$injector'];
+    function translate($injector) {
         var pipTranslate = $injector.has('pipTranslate')
             ? $injector.get('pipTranslate') : null;
         return function (key) {
             return pipTranslate ? pipTranslate.translate(key) || key : key;
         };
     }
-    angular.module('pipList.Translate', [])
-        .filter('translate', filerTranslate);
+    angular
+        .module('pipList.Translate', [])
+        .filter('translate', translate);
 })();
->>>>>>> 9a37eed0d326f682441cc6d15928e372e56139a0
+
 },{}],2:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./dependencies/translate");
 angular.module('pipLists', ['pipTagList']);
+
 },{"./dependencies/translate":1}],3:[function(require,module,exports){
 (function () {
     var TagListController = (function () {
@@ -73,6 +60,7 @@ angular.module('pipLists', ['pipTagList']);
     angular.module('pipTagList', ['pipList.Translate'])
         .component('pipTagList', TagList);
 })();
+
 },{}],4:[function(require,module,exports){
 (function(module) {
 try {
